7bafcaaa0026aba77c80d0476e86f26defbd664b
Gary D. Robinson <gary.robinson2524@gmail.com>
2022-05-13 01:21:57 -0400

    removed SSL

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ config.connection = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  ssl: {
-    ca: serverCa,
-  },
+  //   ssl: {
+  //     ca: serverCa,
+  //   },
 }
 
 config.twilio = {
