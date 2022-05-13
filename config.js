50d87e01a1b292174093a49152567cd81b551959
Gary D. Robinson <gary.robinson2524@gmail.com>
2022-05-13 01:58:48 -0400

    removed filepath

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ var config = {}
 var fs = require('fs')
 const dotenv = require('dotenv')
 dotenv.config()
-const serverCa = [
-  fs.readFileSync(process.env.WEBSITE_PUBLIC_CERTS_PATH, 'utf8'),
-]
+// const serverCa = [
+//   fs.readFileSync(process.env.WEBSITE_PUBLIC_CERTS_PATH, 'utf8'),
+// ]
 
 config.connection = {
   host: process.env.DB_HOST,
