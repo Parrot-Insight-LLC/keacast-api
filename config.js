b82d4f53b07e246807e97b3db303fdc740d68f7e
Gary D. Robinson <gary.robinson2524@gmail.com>
2022-05-12 23:51:27 -0400

    updated certificate path

--- config.js
+++ config.js
@@ -3,10 +3,7 @@ var fs = require('fs')
 const dotenv = require('dotenv')
 dotenv.config()
 const serverCa = [
-  fs.readFileSync(
-    'C:/Users/garobins/Documents/ParrotInsights/keacast-api/DigiCertGlobalRootCA.crt.pem',
-    'utf8',
-  ),
+  fs.readFileSync(process.env.WEBSITE_PUBLIC_CERTS_PATH, 'utf8'),
 ]
 
 config.connection = {
@@ -15,7 +12,7 @@ config.connection = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   ssl: {
-    ca: fs.readFileSync(process.env.WEBSITE_PUBLIC_CERTS_PATH),
+    ca: serverCa,
   },
 }
 
